5122567bcf2f4198b602fe0bd202b46ce9f9e1ad
Glitter-67 <kosei20050607@gmail.com>
2026-03-23 15:16:02 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,20 @@
+from threading import Thread
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+	return {"message": "Server is Online."}
+
+import os
+
+def start():
+	port = int(os.environ.get("PORT", 8080))
+	uvicorn.run(app, host="0.0.0.0", port=port)
+
+def server_thread():
+	t = Thread(target=start)
+	t.start()
